bc23fc48e920f4c8a416985c4716fd0132e0c52a
TournesolTangible <derek.steeg@gmail.com>
2022-02-25 00:20:40 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//
+
+document.getElementsByClassName('btn-header')[0].addEventListener('click', alertUser)
+document.getElementsByClassName('btn-header')[1].addEventListener('click', alertUser)
+
+document.getElementsByClassName('tour-btn')[0].addEventListener('click', alertUser)
+document.getElementsByClassName('tour-btn')[1].addEventListener('click', alertUser)
+document.getElementsByClassName('tour-btn')[2].addEventListener('click', alertUser)
+document.getElementsByClassName('tour-btn')[3].addEventListener('click', alertUser)
+document.getElementsByClassName('tour-btn')[4].addEventListener('click', alertUser)
+document.getElementsByClassName('tour-btn')[5].addEventListener('click', alertUser)
+
+
+function alertUser() {
+    alert('To be implemented!!\n\nThank you for viewing!! \nPlease check the other pages!! ')
+}
